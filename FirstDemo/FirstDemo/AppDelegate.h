f0445469ae050341ff66778abd0f6b80be2ad773
yinmumu <976668255@qq.com>
2016-12-30 10:41:06 +0800

    Second 提交

---
+++ FirstDemo/FirstDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstDemo
+//
+//  Created by BluelansMac on 16/12/30.
+//  Copyright © 2016年 BluelansMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstDemo/FirstDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstDemo
+//
+//  Created by BluelansMac on 16/12/30.
+//  Copyright © 2016年 BluelansMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
